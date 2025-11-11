94f5379d95435f31f62ebdc3a9913b39a9b101d7
yibeltal <yibeltal2514@gmail.com>
2025-11-11 14:03:00 +0300

    fixeda

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import "../src/commonResource/css/bootstrap.css";
 import "../src/commonResource/css/general_css.css";
 import "../src/commonResource/js/jquery-3.7.1.js";
-import ReactDom from "react-DOM/client";
+import ReactDom from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
