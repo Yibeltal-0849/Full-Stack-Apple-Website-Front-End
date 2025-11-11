3b524c4842e99312a3a27a2498856a968714e01c
yibeltal <yibeltal2514@gmail.com>
2025-11-11 14:13:49 +0300

    fixed

--- src/components/body_section/YoutubeVideo/YoutubeVideos.jsx
+++ src/components/body_section/YoutubeVideo/YoutubeVideos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./youTubeVideos.css";
+import "./youtubeVideos.css";
 function YouTubeVideos() {
   const [youTubeVideos, setYouTubeVideos] = useState([]);
 
