369de80662d5e83fc056ed4761d6c2186b059dc5
yibeltal <yibeltal2514@gmail.com>
2025-11-11 14:18:08 +0300

    yes

--- src/components/body_section/YoutubeVideo/YoutubeVideos.jsx
+++ src/components/body_section/YoutubeVideo/YoutubeVideos.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./youtubeVideos.css";
+import "./YoutubeVideos.css";
 function YouTubeVideos() {
   const [youTubeVideos, setYouTubeVideos] = useState([]);
 
